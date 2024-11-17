76d8969e12c27ca99b75a62440dcb8d6f2ae617f
comprock15 <khoruzhaia@sfedu.ru>
2024-11-17 21:17:30 +0300

    Update spawner

--- environment/map/spawner.gd
+++ environment/map/spawner.gd
@@ -11,6 +11,16 @@ var _wait_time: float
 
 var sheep_collection = [sheep, red_sheep, green_sheep, blue_sheep]
 var spawn_points
+var sheep_spawned = 0
+
+var sheep_dict = {
+	"белый": sheep,
+	"красный": red_sheep,
+	"зелёный": green_sheep,
+	"синий": blue_sheep
+}
+
+@onready var order_manager: OrderManager = %OrderManager
 
 
 func _ready() -> void:
@@ -21,12 +31,26 @@ func _ready() -> void:
 
 
 func spawn_sheep() -> void:
+	if not order_manager.is_order_taken():
+		return
 	randomize()
-	sheep_collection.shuffle()
-	for i in range(len(spawn_points)):
-		var instance: Node3D = load(sheep_collection[i % len(sheep_collection)]).instantiate()
+	#sheep_collection.shuffle()
+	#for i in range(len(spawn_points)):
+		#var instance: Node3D = load(sheep_collection[i % len(sheep_collection)]).instantiate()
+		#add_child(instance)
+		#instance.global_transform = spawn_points[i].global_transform
+
+	for s in order_manager.all_ever_requested_sheep.keys():
+		if not s in sheep_dict.keys():
+			continue
+		if order_manager.all_ever_requested_sheep[s] == 0:
+			continue
+		var instance: Node3D = load(sheep_dict[s]).instantiate()
 		add_child(instance)
-		instance.global_transform = spawn_points[i].global_transform
+		var index = sheep_spawned % len(spawn_points)
+		instance.global_transform = spawn_points[index].global_transform
+		sheep_spawned += 1
+	order_manager.reset_all_requested_sheep()
  
 
 func _on_timer_timeout() -> void:
