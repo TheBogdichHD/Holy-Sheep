811265b64ae62d1c42ffa6dcbd5b3b63e2002312
Анна Петросян <apetrosia@sfedu.ru>
2024-11-17 22:55:42 +0300

    feat(music manager): improve sound settings

--- ui/button_manager.gd
+++ ui/button_manager.gd
@@ -5,9 +5,12 @@ func _on_play_button_pressed():
 	get_tree().change_scene_to_file("res://environment/map/map.tscn")
 
 func _on_sound_button_pressed() -> void:
-	MusicManager.volume += 100
-	MusicManager.volume %= 200
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), -MusicManager.volume)
+	MusicManager.music_volume += 100
+	MusicManager.music_volume %= 200
+	MusicManager.sfx_volume += 100
+	MusicManager.sfx_volume %= 200
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), -MusicManager.music_volume)
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("SFX"), -MusicManager.sfx_volume)
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
